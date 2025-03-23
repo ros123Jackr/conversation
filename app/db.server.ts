2a63b38538c73bb66d3f3f8b5b47d50ca15af062
ros123Jackr <Jackrreacher@mail.com>
2025-03-23 21:03:48 +0000

    Fix build - improving security

--- app/db.server.ts
+++ app/db.server.ts
@@ -4,20 +4,20 @@ import invariant from "tiny-invariant";
 import { singleton } from "./singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-let prisma = singleton("prisma", getPrismaClient);
+const prisma = singleton("prisma", getPrismaClient);
 
 function getPrismaClient() {
-  let { DATABASE_URL } = process.env;
+  const { DATABASE_URL } = process.env;
   invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
 
-  let databaseUrl = new URL(DATABASE_URL);
+  const databaseUrl = new URL(DATABASE_URL);
 
-  let isLocalHost = databaseUrl.hostname === "localhost";
+  const isLocalHost = databaseUrl.hostname === "localhost";
 
-  let PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
-  let FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
+  const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
+  const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
 
-  let isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
+  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
 
   if (!isLocalHost) {
     if (databaseUrl.host.endsWith(".internal")) {
@@ -35,7 +35,7 @@ function getPrismaClient() {
   // that this only runs once per server restart and won't automatically be
   // re-run per request like everything else is. So if you need to change
   // something in this file, you'll need to manually restart the server.
-  let client = new PrismaClient({
+  const client = new PrismaClient({
     datasources: {
       db: {
         url: databaseUrl.toString(),
