26de3664a63667850a2c3e65ec2e92766751fb6c
ros123Jackr <Jackrreacher@mail.com>
2025-03-25 22:11:37 +0000

    Improve tests - updating docs

--- app/singleton.server.ts
+++ app/singleton.server.ts
@@ -1,11 +1,11 @@
 // Borrowed & modified from https://github.com/jenseng/abuse-the-platform/blob/main/app/utils/singleton.ts
 // Thanks @jenseng!
 
-export let singleton = <Value>(
+export const singleton = <Value>(
   name: string,
   valueFactory: () => Value,
 ): Value => {
-  let g = global as unknown as { __singletons: Record<string, unknown> };
+  const g = global as unknown as { __singletons: Record<string, unknown> };
   g.__singletons ??= {};
   g.__singletons[name] ??= valueFactory();
   return g.__singletons[name] as Value;
